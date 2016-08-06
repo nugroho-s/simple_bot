ed8c5b914d07b14f173aa9aaf8aace72149700dc
nugroho-s <massatriya@gmail.com>
2016-08-06 15:31:42 +0700

    add pixel detection

--- simple_bot/simple_bot/Form1.cs
+++ simple_bot/simple_bot/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,9 +15,47 @@ namespace simple_bot
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void MouseMoveTimer_Tick(object sender, EventArgs e)
+        {
+            Point cursor = new Point();
+            GetCursorPos(ref cursor);
+
+            var c = GetColorAt(cursor);
+            this.BackColor = c;
+
+            if (c.R == c.G && c.G < 64 && c.B > 128)
+            {
+                MessageBox.Show("Blue");
+            }
+        }
+
+        Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        public Color GetColorAt(Point location)
+        {
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -27,6 +67,12 @@ namespace simple_bot
         private void button2_Click(object sender, EventArgs e)
         {
             MessageBox.Show(Bot.CurrentMouse().ToString(),"current loc");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Thread.Sleep(5000);
+            MessageBox.Show((GetColorAt(new Point(0,0))).ToString(), "pixel test");
         }
     }
 }
